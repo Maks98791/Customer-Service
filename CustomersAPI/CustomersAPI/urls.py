bcaeddd2356e162c5649d38ea6668feb992c3f6a
Maks98791 <maksim.birszel@gmail.com>
2020-08-03 23:20:25 +0200

    add customer model to admin page, create superuser and update paths for api

--- CustomersAPI/CustomersAPI/urls.py
+++ CustomersAPI/CustomersAPI/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('api.urls')),
+    path('', include('api.urls')),
 ]
--- CustomersAPI/api/urls.py
+++ CustomersAPI/api/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path, include
 from api import views
 
 
 urlpatterns = [
-    url(r'^customers/$', views.customer_list),
-    url(r'^customers/(P&lt;pk&gt;[0-9]+)$', views.customer_details),
-    url(r'^customers/age/(P&lt;age&gt;[0-9]+)/$', views.customer_list_by_age),
+    path('customers/', views.customer_list),
+    path('customers/<pk>/', views.customer_details),
+    path('customers/age/<age>', views.customer_list_by_age),
 ]
