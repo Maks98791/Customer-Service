de11a997972f92f79b198534fd1b7ae1e0e30d6e
Maks98791 <maksim.birszel@gmail.com>
2020-08-03 23:25:11 +0200

    add default redirect for empty path

--- CustomersAPI/api/urls.py
+++ CustomersAPI/api/urls.py
@@ -3,7 +3,8 @@ from api import views
 
 
 urlpatterns = [
-    path('customers/', views.customer_list),
-    path('customers/<pk>/', views.customer_details),
-    path('customers/age/<age>', views.customer_list_by_age),
+    path('', views.customer_list),
+    path('api/customers/', views.customer_list),
+    path('api/customers/<pk>/', views.customer_details),
+    path('api/customers/age/<age>', views.customer_list_by_age),
 ]
